20b6c1d94ad1be3852144335080666701983bf7e
astro1sumit <astro.sumit.usa@gmail.com>
2025-06-18 09:54:19 +0530

    task 2 done

---
+++ task 2/script.js
@@ -0,0 +1,11 @@
+
+
+const notecontainer = document.getElementById('notes');
+
+notecontainer.addEventListener('click',()=>{
+    <input type="text" id="note-input" placeholder="Type your note here..."></input>
+    const noteinput = document.getElementById('note-input');
+    const newNote = document.createElement('div');
+    
+
+})
